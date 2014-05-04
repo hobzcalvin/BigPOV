95507fe5fba5b16991bd4c2f4acbe79a4bb3d363
Grant Patterson <grant.patterson@gmail.com>
2014-05-03 23:03:00 -0700

    Finishes for IFTF

--- bigpov.py
+++ bigpov.py
@@ -5,17 +5,23 @@ import urllib, cStringIO, errno
 import RPi.GPIO as gpio
 import cPickle as pickle
 import threading
+import atexit
+import datetime
 
 # Configurable values
 fromTop       = False
 dev           = "/dev/spidev0.0"
 skip          = 0
-skipAfter     = 18
+skipAfter     = 32
 length        = 92 #106 #218 #326
 displaytime   = 5
 redButton     = 14
 redButtonFile = "rose.jpg"
 correctStart  = 5000
+obey_shutdown = False
+
+light_start = datetime.time(4)
+light_end = datetime.time(20)
 
 # Setup GPIO pins
 gpio.setmode(gpio.BCM)
@@ -50,13 +56,37 @@ except OSError as exc:
   else: raise
 
 l = threading.Lock()
+targettime = time.time()
 display_columns = None
 display_repeating = False
 display_updated = False
 quitting = False
 
+def show_instagram():
+  try:
+    #print "showing instagram?"
+    url = urllib.urlopen("http://selfobserved.org/instagram.php")
+    if url.getcode() != 200:
+      #print "  no image this time."
+      return False
+    filename = "_from_insta_" + url.geturl().split('/')[-1]
+    #print "  got", url.geturl()
+    filepath = os.path.join(imgpath, filename)
+    if os.path.isfile(filepath):
+      os.remove(filepath)
+      #print "  but we already have it!"
+      return False
+    f = open(filepath, 'w')
+    f.write(url.read())
+    f.close()
+    #print "  showing it"
+    displayFile(filename)
+    return True
+  except IOError:
+    return False
 
 def displayFile(file):
+  global targettime
   global display_columns
   global display_repeating
   global display_updated
@@ -64,77 +94,55 @@ def displayFile(file):
   repeating = "gggtiled" in file
   try:
     #print "this one is %s" % file
-    if file == "url":
-      #print "it's a url!";
-      f = cStringIO.StringIO(urllib.urlopen("http://selfobserved.org/instagram.php").read())
-      img = Image.open(f)
-    else:
-      cacheName = os.path.join(cachepath, str(length) + '_' + str(int(fromTop)) + '_' + str(skip) + '_' + str(skipAfter) + '.' + file)
-      try:
-        cacheFile = open(cacheName, 'rb')
-        columns = pickle.load(cacheFile)
-        cacheFile.close()
-        #width = len(columns)
-        #print "  cache load of %s worked!" % cacheName
-      except:
-        #print '  loading manually'
-        img = Image.open(os.path.join(imgpath, file))
-        if img.size[1] > length or img.size[1] < length * 0.9:
-          newsize = tuple([int(float(s) / img.size[1] * length) for s in img.size])
-          #print "  size is %dx%d; resizing to %dx%d!" % (img.size + newsize)
-          img = img.resize(newsize, Image.ANTIALIAS)
-        pixels = img.convert("RGB").load()
-        width = img.size[0]
-        height = img.size[1]
+    cacheName = os.path.join(cachepath, str(length) + '_' + str(int(fromTop)) + '_' + str(skip) + '_' + str(skipAfter) + '.' + file)
+    try:
+      cacheFile = open(cacheName, 'rb')
+      columns = pickle.load(cacheFile)
+      cacheFile.close()
+      #width = len(columns)
+      #print "  cache load of %s worked!" % cacheName
+    except:
+      #print '  loading manually'
+      img = Image.open(os.path.join(imgpath, file))
+      if img.size[1] > length or img.size[1] < length * 0.9:
+        newsize = tuple([int(float(s) / img.size[1] * length) for s in img.size])
+        #print "  size is %dx%d; resizing to %dx%d!" % (img.size + newsize)
+        img = img.resize(newsize, Image.ANTIALIAS)
+      pixels = img.convert("RGB").load()
+      width = img.size[0]
+      height = img.size[1]
 
-        columns = [0 for x in range(width)]
-        for x in range(width):
-          columns[x] = bytearray([0x80] * (skip + length + skipAfter) * 3 + [0] * 30)
-          #columns[x] = bytearray([0x80] * skip * 3 + [0] * (length * 3 + 30) + [0x80] * skipAfter * 3)
-          for y in range(height):
-            value = pixels[x, y]
-            colY = y if fromTop else height - y - 1
-            columns[x][skip * 3 + colY * 3 + 1] = gamma[value[0]]
-            if (skip + colY) < correctStart:
-              columns[x][skip * 3 + colY * 3 + 2] = gamma[value[1]]
-              columns[x][skip * 3 + colY * 3 + 0] = gamma[value[2]]
-            else:
-              columns[x][skip * 3 + colY * 3 + 2] = gbCorrectGamma[value[1]]
-              columns[x][skip * 3 + colY * 3 + 0] = gbCorrectGamma[value[2]]
+      columns = [0 for x in range(width)]
+      for x in range(width):
+        columns[x] = bytearray([0x80] * (skip + length + skipAfter) * 3 + [0] * 30)
+        #columns[x] = bytearray([0x80] * skip * 3 + [0] * (length * 3 + 30) + [0x80] * skipAfter * 3)
+        for y in range(height):
+          value = pixels[x, y]
+          colY = y if fromTop else height - y - 1
+          columns[x][skip * 3 + colY * 3 + 1] = gamma[value[0]]
+          if (skip + colY) < correctStart:
+            columns[x][skip * 3 + colY * 3 + 2] = gamma[value[1]]
+            columns[x][skip * 3 + colY * 3 + 0] = gamma[value[2]]
+          else:
+            columns[x][skip * 3 + colY * 3 + 2] = gbCorrectGamma[value[1]]
+            columns[x][skip * 3 + colY * 3 + 0] = gbCorrectGamma[value[2]]
 
-        cacheFile = open(cacheName, 'wb')
-        pickle.dump(columns, cacheFile, pickle.HIGHEST_PROTOCOL)
-        cacheFile.close()
-        #print '  successfully saved cache', cacheName
+      cacheFile = open(cacheName, 'wb')
+      pickle.dump(columns, cacheFile, pickle.HIGHEST_PROTOCOL)
+      cacheFile.close()
+      #print '  successfully saved cache', cacheName
 
+    # Wait for current image to have been displayed for targettime
+    while time.time() < targettime and not quitting:
+      time.sleep(0.1)
     l.acquire()
     display_columns = columns
     display_repeating = repeating
     display_updated = True
-    print "updated display"
     l.release()
+    # Set new targettime
     targettime = time.time() + displaytime
-    while time.time() < targettime:
-      time.sleep(0.1)
-    """targettime = time.time() + displaytime
-    # 1330 FPS for length=326
-    while time.time() < targettime:
-      '''if gpio.event_detected(redButton):
-        print "button press", time.time()
-        displayFile(redButtonFile)
-        break'''
-      for x in range(width):
-        spidev.write(columns[x])
-        spidev.flush()
-      if repeating:
-        continue
-      for x in range(width):
-        spidev.write(columns[width - x - 1])
-        spidev.flush()"""
 
-    # Clear
-    #spidev.write(clearBytes)
-    #spidev.flush()
   except IOError:
     print "%s is not a valid image" % file
 
@@ -142,11 +150,12 @@ def display_worker():
   global display_updated
   while True:
     l.acquire()
-    print "getting new columns"
     columns = display_columns
     repeating = display_repeating
     l.release()
     if columns is None:
+      spidev.write(clearBytes)
+      spidev.flush()
       time.sleep(0.1)
       continue
     width = len(columns)
@@ -161,8 +170,22 @@ def display_worker():
         spidev.flush()
     display_updated = False
     if quitting:
-      print "i'm outta here"
       break
+
+def shutdown():
+  global display_updated
+  global quitting
+  quitting = True
+  display_updated = True
+  dw.join()
+  spidev.write(clearBytes)
+  spidev.flush()
+  print "\nbye"
+  sys.exit(0)
+
+def check_shutdown():
+  if obey_shutdown and time.time() >= shutdownTime:
+    shutdown()
 
 # MAIN LOOP
 
@@ -170,42 +193,35 @@ dw = threading.Thread(target=display_worker)
 dw.daemon = True
 dw.start()
 
-
 try:
   # 10 hours between civil twilights, and give us 15 minutes before power off.
   shutdownTime = time.time() + 60 * (60 * 10 - 15)
 
   while True:
+
+    while light_start < datetime.datetime.now().time() < light_end:
+      l.acquire()
+      display_columns = None
+      display_updated = True
+      l.release()
+      time.sleep(10)
+
     # Iterate over files in [this file]/images/
     files = [ f for f in os.listdir(imgpath)
       if os.path.isfile(os.path.join(imgpath,f)) ]
     random.shuffle(files)
 
     for file in files:
-      if False and time.time() >= shutdownTime:
-        print "initiating shutdown!"
-        quitting = True
-        display_updated = True
-        dw.join()
-        print "joined the worker; clearing"
-        spidev.write(clearBytes)
-        spidev.flush()
-        os.system("sudo shutdown -h now")
-        sys.exit(0)
-
-      # Otherwise, display this image
       displayFile(file)
+      # Shutdown if we should
+      check_shutdown()
+      # Show Instagram images while we can
+      while show_instagram():
+        check_shutdown()
 
 except KeyboardInterrupt:
-  print "initiating shutdown!"
-  quitting = True
-  display_updated = True
-  dw.join()
-  print "joined the worker; clearing"
-  spidev.write(clearBytes)
-  spidev.flush()
-  print "\nbye"
-  sys.exit(0)
+  shutdown()
+
 
 '''
 clearBytes = bytearray([0x80] * (skip + length) * 3 + [0] * 10)
